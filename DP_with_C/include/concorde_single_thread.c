9291c630852694cdf547e8688706714cd25ce701
ckckdud3 <ckckdud123@gmail.com>
2023-09-12 15:14:23 +0900

    Local branch test #2

--- DP_with_C/include/concorde_single_thread.c
+++ DP_with_C/include/concorde_single_thread.c
@@ -15,6 +15,7 @@ PointArray PA_init(int num_nodes) {
     ret->points = (Point)malloc(sizeof(_Point)*num_nodes);
 
     for(int i=0; i<num_nodes; i++) {
+        // -1.0 Indicates that points are null.
         (ret->points[i]).x = -1.0;
         (ret->points[i]).y = -1.0;
     }
